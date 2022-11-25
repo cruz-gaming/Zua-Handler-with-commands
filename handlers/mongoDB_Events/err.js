12bb8eb7d0aee6446951b2cfa147cdf82c434bbd
RJRYT <robinjratr@gmail.com>
2022-11-25 18:23:51 +0530

    Update err.js

--- handlers/mongoDB_Events/err.js
+++ handlers/mongoDB_Events/err.js
@@ -6,12 +6,3 @@ module.exports = {
   }
 }
 
-/**
- * @INFO
- * Bot Coded by Zedro#2742 | https://discord.gg/milanio
- * @INFO
- * Work for Milanio Development | https://discord.gg/milanio
- * @INFO
- * Please Mention Us Milanio Development, When Using This Code!
- * @INFO
- */
